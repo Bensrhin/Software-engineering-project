19ca1dcfc80dc15d592ed577b328371756f5dfc6
Nabil Bensrhier <nabil.bensrhier@grenoble-inp.org>
2020-01-13 01:06:41 +0100

    Update B: LisInst

--- src/main/java/fr/ensimag/deca/tree/ListInst.java
+++ src/main/java/fr/ensimag/deca/tree/ListInst.java
@@ -1,6 +1,8 @@
 package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.context.Type;
+
+import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
@@ -9,7 +11,7 @@ import fr.ensimag.deca.tools.IndentPrintStream;
 import fr.ensimag.ima.pseudocode.Label;
 
 /**
- * 
+ *
  * @author gl53
  * @date 01/01/2020
  */
@@ -19,15 +21,21 @@ public class ListInst extends TreeList<AbstractInst> {
      * Implements non-terminal "list_inst" of [SyntaxeContextuelle] in pass 3
      * @param compiler contains "env_types" attribute
      * @param localEnv corresponds to "env_exp" attribute
-     * @param currentClass 
+     * @param currentClass
      *          corresponds to "class" attribute (null in the main bloc).
      * @param returnType
      *          corresponds to "return" attribute (void in the main bloc).
-     */    
+     */
     public void verifyListInst(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass, Type returnType)
             throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+        Iterator<AbstractInst> insts = this.iterator();
+        while (insts.hasNext())
+        {
+            AbstractInst inst = insts.next();
+            inst.verifyInst(compiler, localEnv, currentClass, returnType);
+        }
+        
     }
 
     public void codeGenListInst(DecacCompiler compiler) {
