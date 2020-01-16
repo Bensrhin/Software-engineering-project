ab1d8cdbd9474ff8477df37eb7f3503009fb3049
Nabil Bensrhier <nabil.bensrhier@grenoble-inp.org>
2020-01-16 23:48:42 +0100

    Update tests

--- tester.py
+++ tester.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 import os
 
-
-print(" Que Voulez-Vous Tester ? [0 (Lexer) / 1 (Parser)] / [2 (Context)] / [3 (All), mais bon y'a bcp !!]")
-x = int(input())
-if x == 0:
-    os.system("./test_lexer.py")
-elif x == 1:
-    os.system("./test_synt.py")
-elif x==2:
-    os.system("./test_context.py")
-elif x==3:
-    print("test_lexer")
-    os.system("./test_lexer.py")
-    print("test_synt")
-    os.system("./test_synt.py")
-    print("test_context")
-    os.system("./test_context.py")
-
-
-
+while(1):
+    print("Que Voulez-Vous Tester ? \n\n" +
+    " [1 (Lexer)] \n [2 (Parser)] \n [3 (Context)] \n [4 (All)] \n [0 (Quitter)]\n")
+    # x = int(input())
+    try:
+        x = int(input("Veuillez choisir un nombre entre 0 et 4: "))
+    except SyntaxError:
+        print("\nNot an integer! Try again.\n")
+        continue
+    except NameError:
+        print("\nNot an integer! Try again.\n")
+    else:
+        if (x not in [0, 1, 2, 3, 4]):
+            continue
+        if x == 0:
+            break
+        if x == 1:
+            os.system("./test_lexer.py")
+        elif x == 2:
+            os.system("./test_synt.py")
+        elif x==3:
+            os.system("./test_context.py")
+        elif x==4:
+            print("test_lexer")
+            os.system("./test_lexer.py")
+            print("test_synt")
+            os.system("./test_synt.py")
+            print("test_context")
+            os.system("./test_context.py")
