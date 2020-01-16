ab73cb44b878971dfe9ce0ea72a54b4d5349a4ee
Kacem Jedoui <kacem.jedoui@grenoble-inp.org>
2020-01-16 22:04:31 +0100

    Generateur de tests

--- tester.py
+++ tester.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 import os
 
-def main():
-    print(" Que Voulez-Vous Tester ? [0 (Lexer) / 1 (Parser)]")
-    x = int(input())
-    if x == 0:
-        os.system("./test_lexer.py")
-    elif x == 1:
-        os.system("./test_synt.py")
+
+print(" Que Voulez-Vous Tester ? [0 (Lexer) / 1 (Parser)] / [3 (Context)] / [4 (All), mais bon y'a bcp !!]")
+x = int(input())
+if x == 0:
+    
+    os.system("./test_lexer.py")
+elif x == 1:
+    os.system("./test_synt.py")
+elif x==2:
+    os.system("./test_context.py")
+elif x==4:
+    print("test_lexer")
+    os.system("./test_lexer.py")
+    print("test_synt")
+    os.system("./test_synt.py")
+    print("test_context")
+    os.system("./test_context.py")
 
 
-main()
+
