fb70a6bc3876ba2aa402877a508834604cb513ca
Nabil Bensrhier <nabil.bensrhier@grenoble-inp.org>
2020-01-16 16:28:34 +0100

    Update B: Cast Done

---
+++ src/main/java/fr/ensimag/deca/tree/Extends.java
@@ -0,0 +1,277 @@
+package fr.ensimag.deca.tree;
+import java.util.*;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.context.ClassType;
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.Definition;
+import fr.ensimag.deca.context.IntType;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.FieldDefinition;
+import fr.ensimag.deca.context.MethodDefinition;
+import fr.ensimag.deca.context.TypeDefinition;
+import fr.ensimag.deca.context.ExpDefinition;
+import fr.ensimag.deca.context.VariableDefinition;
+import fr.ensimag.deca.tools.DecacInternalError;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import fr.ensimag.deca.tools.SymbolTable.Symbol;
+import java.io.PrintStream;
+import org.apache.commons.lang.Validate;
+import org.apache.log4j.Logger;
+import fr.ensimag.ima.pseudocode.Register;
+import fr.ensimag.ima.pseudocode.RegisterOffset;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import fr.ensimag.ima.pseudocode.instructions.WINT;
+import fr.ensimag.ima.pseudocode.instructions.WFLOAT;
+import fr.ensimag.ima.pseudocode.GPRegister;
+
+/**
+ * Deca Extends
+ *
+ * @author gl53
+ * @date 01/01/2020
+ */
+public class Extends extends AbstractIdentifier {
+
+    @Override
+    protected void checkDecoration() {
+        if (getDefinition() == null) {
+            throw new DecacInternalError("Extends " + this.getName() + " has no attached Definition");
+        }
+    }
+
+    @Override
+    public Definition getDefinition() {
+        return definition;
+    }
+
+    /**
+     * Like {@link #getDefinition()}, but works only if the definition is a
+     * ClassDefinition.
+     *
+     * This method essentially performs a cast, but throws an explicit exception
+     * when the cast fails.
+     *
+     * @throws DecacInternalError
+     *             if the definition is not a class definition.
+     */
+    @Override
+    public ClassDefinition getClassDefinition() {
+        try {
+            return (ClassDefinition) definition;
+        } catch (ClassCastException e) {
+            throw new DecacInternalError(
+                    "Identifier "
+                            + getName()
+                            + " is not a class identifier, you can't call getClassDefinition on it");
+        }
+    }
+
+    /**
+     * Like {@link #getDefinition()}, but works only if the definition is a
+     * MethodDefinition.
+     *
+     * This method essentially performs a cast, but throws an explicit exception
+     * when the cast fails.
+     *
+     * @throws DecacInternalError
+     *             if the definition is not a method definition.
+     */
+    @Override
+    public MethodDefinition getMethodDefinition() {
+        try {
+            return (MethodDefinition) definition;
+        } catch (ClassCastException e) {
+            throw new DecacInternalError(
+                    "Identifier "
+                            + getName()
+                            + " is not a method identifier, you can't call getMethodDefinition on it");
+        }
+    }
+
+    /**
+     * Like {@link #getDefinition()}, but works only if the definition is a
+     * FieldDefinition.
+     *
+     * This method essentially performs a cast, but throws an explicit exception
+     * when the cast fails.
+     *
+     * @throws DecacInternalError
+     *             if the definition is not a field definition.
+     */
+    @Override
+    public FieldDefinition getFieldDefinition() {
+        try {
+            return (FieldDefinition) definition;
+        } catch (ClassCastException e) {
+            throw new DecacInternalError(
+                    "Identifier "
+                            + getName()
+                            + " is not a field identifier, you can't call getFieldDefinition on it");
+        }
+    }
+
+    /**
+     * Like {@link #getDefinition()}, but works only if the definition is a
+     * VariableDefinition.
+     *
+     * This method essentially performs a cast, but throws an explicit exception
+     * when the cast fails.
+     *
+     * @throws DecacInternalError
+     *             if the definition is not a field definition.
+     */
+    @Override
+    public VariableDefinition getVariableDefinition() {
+        try {
+            return (VariableDefinition) definition;
+        } catch (ClassCastException e) {
+            throw new DecacInternalError(
+                    "Identifier "
+                            + getName()
+                            + " is not a variable identifier, you can't call getVariableDefinition on it");
+        }
+    }
+
+    /**
+     * Like {@link #getDefinition()}, but works only if the definition is a ExpDefinition.
+     *
+     * This method essentially performs a cast, but throws an explicit exception
+     * when the cast fails.
+     *
+     * @throws DecacInternalError
+     *             if the definition is not a field definition.
+     */
+    @Override
+    public ExpDefinition getExpDefinition() {
+        try {
+            return (ExpDefinition) definition;
+        } catch (ClassCastException e) {
+            throw new DecacInternalError(
+                    "Identifier "
+                            + getName()
+                            + " is not a Exp identifier, you can't call getExpDefinition on it");
+        }
+    }
+
+    @Override
+    public void setDefinition(Definition definition) {
+        this.definition = definition;
+    }
+
+    @Override
+    public Symbol getName() {
+        return name;
+    }
+
+    private Symbol name;
+
+    public Extends(Symbol name) {
+        Validate.notNull(name);
+        this.name = name;
+    }
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass) throws ContextualError {
+        /*
+        Set<Symbol> sym = localEnv.stringIsIn();
+        for (Symbol s:sym)
+        {
+            System.out.println(s.getName());
+        }
+        */
+        //System.out.println(this.getName().getName());
+        Symbol s = localEnv.stringIsIn(this.getName());
+        if (s == null)
+        {
+                throw new ContextualError("\nIdentifier "
+                        + this.getName().toString()
+                        + " is unknown", this.getLocation());
+        }
+        ExpDefinition def = localEnv.get(s);
+        this.setDefinition(def);
+        this.setType(def.getType());
+        return this.getType();
+    }
+
+    /**
+     * Implements non-terminal "type" of [SyntaxeContextuelle] in the 3 passes
+     * @param compiler contains "env_types" attribute
+     */
+    @Override
+    public Type verifyType(DecacCompiler compiler) throws ContextualError {
+      if (!compiler.getSymbols().checkSymbol(this.getName().toString())){
+              throw new ContextualError(this.getName().toString()
+                      + " Type is not yet implemented", this.getLocation());
+          }
+
+          Type type = this.getName().getType();
+          //ExpDefinition def = this.getName().
+          TypeDefinition def = new TypeDefinition(type, Location.BUILTIN);
+          this.setDefinition(def);
+          this.setType(type);
+          return this.getType();
+    }
+
+
+    private Definition definition;
+
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        // leaf node => nothing to do
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        // leaf node => nothing to do
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        s.print(name.toString());
+    }
+
+    @Override
+    String prettyPrintNode() {
+        return "Identifier (" + getName() + ")";
+    }
+
+    @Override
+    protected void prettyPrintType(PrintStream s, String prefix) {
+        Definition d = getDefinition();
+        if (d != null) {
+            s.print(prefix);
+            s.print("definition: ");
+            s.print(d);
+            s.println();
+        }
+    }
+    @Override
+    public void codeGenIdent(DecacCompiler compiler,int i){
+        RegisterOffset r = new RegisterOffset(i, Register.GB);
+        ExpDefinition def = this.getExpDefinition();
+        def.setOperand(r);
+    }
+    @Override
+     protected void codeGenPrint(DecacCompiler compiler) {
+        compiler.addInstruction(new LOAD(this.getExpDefinition().getOperand(), Register.R1));
+        if(this.getType().toString().equals("int")){
+            compiler.addInstruction(new WINT());
+            
+        }
+        if(this.getType().toString().equals("float")){
+            compiler.addInstruction(new WFLOAT());
+            
+        }
+        //throw new UnsupportedOperationException("not yet implemented55");
+    }
+    @Override
+    protected void codeGenLoad(DecacCompiler compiler, GPRegister r1) {
+        //throw new UnsupportedOperationException("not yet implemented55");
+        compiler.addInstruction(new LOAD(this.getExpDefinition().getOperand(), r1));
+    }
+
+}
