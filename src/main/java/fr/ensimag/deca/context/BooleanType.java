fd5630abae2c7cebe718290c2e3298e6e626fe7e
Nabil Bensrhier <nabil.bensrhier@grenoble-inp.org>
2020-01-16 21:11:16 +0100

    Update tests Context

--- src/main/java/fr/ensimag/deca/context/BooleanType.java
+++ src/main/java/fr/ensimag/deca/context/BooleanType.java
@@ -23,7 +23,11 @@ public class BooleanType extends Type {
 
     @Override
     public boolean sameType(Type otherType) {
-        throw new UnsupportedOperationException("not yet implemented");
+        if (otherType.isBoolean())
+        {
+          return true;
+        }
+        return false;
     }
 
 
