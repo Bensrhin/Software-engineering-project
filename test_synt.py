b96f2f8d173dc84f33bcfc0b379e5cf8f6b7b973
Redouane Yagouti <redouane.yagouti@grenoble-inp.org>
2020-01-16 17:27:45 +0100

    script synt .py

--- test_synt.py
+++ test_synt.py
@@ -17,14 +17,16 @@ def files(path):
 
 
 def valid_synt():
-    counter = 0;
+    counter = 0
+    tmp = 0
     test_synt = "src/test/script/launchers/test_synt"
     valid_synt = "src/test/deca/syntax/valid"
     print("Details d'execution [1/0] ?")
     x = int(input())
     print("======================================================")
-    #for file in os.listdir(valid_synt):
+
     for file in files(valid_synt):
+        tmp += 1
         execute = test_synt + " " + valid_synt + "/" + str(file) + " " +"2> {}.log".format(str(file))
         if x == 1:
             os.system(execute)
@@ -33,25 +35,29 @@ def valid_synt():
             os.system("rm synt.txt")
         if os.stat("{}.log".format(str(file))).st_size != 0:
             print(file+color.BOLD+color.WARNING+"  *** [Test FAILED UNEXPECTED] *** "+color.ENDC)
-            counter+=1
+
         else:
             print(file+color.BOLD+color.HEADER+"  *** [TEST PASSED EXPECTED] *** "+color.ENDC)
+            counter+=1
         print("======================================================")
-    return counter
+    return counter, tmp
 
 
 def invalid_synt():
-    counter = 0;
+    counter = 0
+    tmp = 0
     test_synt = "src/test/script/launchers/test_synt"
     invalid_synt = "src/test/deca/syntax/invalid"
     print("~======================================================~")
     #for file in os.listdir(invalid_synt):
-    for file in files (invalid_synt):
+    for file in files(invalid_synt):
+        tmp += 1
         execute = test_synt + " " + invalid_synt + "/" + str(file) + " " +"2> {}.log".format(str(file))
         #print(execute)
         os.system(execute)
         #os.open(format(str(file))+".log",os.O_RDWR)
         if os.stat("{}.log".format(str(file))).st_size != 0:
+            counter += 1
             fichier = open("{}.log".format(str(file)), "r")
             read = fichier.readlines()[0]
             if read[0] == 's':
@@ -61,25 +67,25 @@ def invalid_synt():
             print(new_read+color.BOLD+color.HEADER +"  *** [Test FAILED EXPECTED] ***"+color.ENDC)
         else:
             print(file+color.BOLD+color.WARNING+"  *** [TEST PASSED UNEXPECTED] ***"+color.ENDC)
-            counter+=1
+
         print("~======================================================~")
-    return counter
+    return counter, tmp
 
 
 
 y = valid_synt()
-if (y==0):
-    print(color.BOLD+ color.OKGREEN+"     .-~-.-~-.-~[TEST VALID SUCCESS].-~-.-~-.-~"+color.ENDC)
+if (y[0] == y[1]):
+    print(color.BOLD+ color.OKGREEN+"     .-~-.-~-.-~[{} TESTS VALID SUCCESS].-~-.-~-.-~".format(str(y[1]))+color.ENDC)
     print("~======================================================~")
-if (y!=0):
-    print(color.BOLD+ color.FAIL+"     .-~-.-~-.-~[TEST VALID ERROR].-~-.-~-.-~"+color.ENDC)
+else:
+    print(color.BOLD+ color.FAIL+"     .-~-.-~-.-~[{} TESTS VALID ERROR].-~-.-~-.-~".format(str(y[1] -y[0]))+color.ENDC)
     print("~========================================================================~")
 x = invalid_synt()
-if (x==0):
-    print(color.BOLD+ color.OKGREEN+"     .-~-.-~-.-~[TEST INVALID SUCCESS].-~-.-~-.-~"+color.ENDC)
+if (x[0] == x[1]):
+    print(color.BOLD+ color.OKGREEN+"     .-~-.-~-.-~[{} TESTS INVALID SUCCESS].-~-.-~-.-~".format(str(x[1]))+color.ENDC)
     print("~======================================================~")
-if (x!=0):
-    print(color.BOLD+ color.FAIL+"     .-~-.-~-.-~[TEST INVALID ERROR].-~-.-~-.-~"+color.ENDC)
+else:
+    print(color.BOLD+ color.FAIL+"     .-~-.-~-.-~[{} TESTS INVALID ERROR].-~-.-~-.-~".format(str(x[1] - x[0]))+color.ENDC)
     print("~========================================================================~")
-
+#
 os.system("rm *.log")
